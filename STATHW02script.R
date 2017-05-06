384ecd2e371c80acef34a17899a37bddf577f37c
dannyj4 <ds1383a@student.american.edu>
2017-05-06 19:43:03 -0400

    Add files via upload

--- STATHW02script.R
+++ STATHW02script.R
@@ -10,3 +10,7 @@ read.table("Baltimore_City_Employee_Salaries_FY2015.csv" , fill = TRUE )
 #method of achieving destrung list of values from column 7
 AnnualSalarycol6 <- as.numeric(unlist(dannysframe[6]))
 barplot(AnnualSalarycol6)
+
+getwd(
+  
+)
